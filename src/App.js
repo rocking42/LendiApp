e243a2982a7cfc3d1f87c0536f4d2d956368ea7d
rocking42 <nedpike3@gmail.com>
2017-02-21 02:38:45 +1100

    Added endless scroll

--- src/App.js
+++ src/App.js
@@ -7,23 +7,37 @@ import { Album } from './Album';
 class App extends Component {
   constructor (props) {
     super(props);
+
     this.state = {
-      albums: []
-    }
+      albums: [],
+      sampleAlbums: []
+    };
+
+    this.scrollUpdate = this.scrollUpdate.bind(this);
   }
 
   componentDidMount () {
-    let items;
+    window.addEventListener('scroll', this.scrollUpdate);
     axios.get('https://jsonplaceholder.typicode.com/photos')
       .then((res) => {
+        // All albums are kept in state as well as a sample of all albums
         this.setState({
-          albums: res.data
+          albums: res.data,
+          sampleAlbums: res.data.slice(0, 10)
         })
       });
   }
+  // Decided on an endless scroll as rendering 10000 elements at once added performance issues
+  scrollUpdate(e) {
+      if(document.documentElement.scrollHeight - e.currentTarget.scrollY < 1000) {
+        this.setState({
+          sampleAlbums: this.state.albums.slice(0, this.state.sampleAlbums.length + 10)
+        })
+      };
+  }
 
   render () {
-      const allAlbums = this.state.albums.map((album) => {
+      const allAlbums = this.state.sampleAlbums.map((album) => {
         return (
           <Album
             thumbnail={album.thumbnailUrl}
